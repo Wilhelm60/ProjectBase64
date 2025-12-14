40506b6ae924c783af654e3e29eb4f3c9b1bdbd8
Wilhelm60 <63043331+Wilhelm60@users.noreply.github.com>
2025-12-15 01:46:59 +0300

    Delete FileManager.h

--- FileManager.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-
-class FileManager {
-public:
-    static std::string read(const std::string& path);
-    static void write(const std::string& path, const std::string& text);
-};
